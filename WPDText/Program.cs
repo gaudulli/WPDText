64d6e88d1f9624e6393f4f37b90b33e689033e6b
Ric.Gaudet <Ric.Gaudet@LCS170032>
2019-05-21 16:25:34 -0600

    Add project files.

---
+++ WPDText/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace WPDText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<WPDFile> files = new List<WPDFile>();
+            string url = @"path\\to\\wpd2.0";
+
+            DirectoryInfo d = new DirectoryInfo(url);
+            FileInfo[] f = d.GetFiles();
+            foreach (FileInfo file in f)
+            {
+                string text = WPD_TextParser.Parse(file.FullName);
+                files.Add(new WPDFile(file.Name, text));
+            }
+        }
+    }
+
+
+    public struct WPDFile
+    {
+        public string fileName;
+        public string text;
+
+        public WPDFile(string _fileName, string _text)
+        {
+            fileName = _fileName;
+            text = _text;
+        }
+    }
+}
---
+++ WPDText/WPD_TextParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tokenizer;
+
+namespace WPDText
+{
+    public static class WPD_TextParser
+    {
+
+        public static string Parse(string url)
+        {
+                TokenizedDocument doc = new TokenizedDocument(url);
+                return UtilityMethods.ConvertTokensToString(doc.DocumentTokens, 0);         
+        }
+
+
+
+
+    }
+}
